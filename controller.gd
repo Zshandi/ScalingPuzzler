4ebbbddb078c605cffa9b4c4915fe6684ce7c83c
Zshandi Krahn <ZshandiK@gmail.com>
2024-08-17 16:46:55 -0700

    Move transformation target to single variable

--- controller.gd
+++ controller.gd
@@ -4,6 +4,10 @@ extends Node2D
 ## The distance which corresponds to doubling or halving the scale
 var scale_factor := 500.0
 
+@export
+## The node to apply transform controls to by default
+var transform_target:Transformable
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -25,20 +29,20 @@ func handle_scaling(delta):
 		if !is_scaling:
 			scale_origin = current_cursor_pos
 			# Assumes equal x and y scale
-			assert($ScalableRoomInner.global_transform.get_scale().x ==
-				$ScalableRoomInner.global_transform.get_scale().y)
-			original_scale = $ScalableRoomInner.get_current_linear_scale()
+			assert(transform_target.global_transform.get_scale().x ==
+				transform_target.global_transform.get_scale().y)
+			original_scale = transform_target.get_current_linear_scale()
 			is_scaling = true
 		else:
 			var scale_distance := -(current_cursor_pos.y - scale_origin.y) / scale_factor
 			
-			var current_scale:float = $ScalableRoomInner.get_current_linear_scale()
+			var current_scale:float = transform_target.get_current_linear_scale()
 			
 			var current_distance := current_scale - original_scale
 			
 			var distance_to_go := scale_distance - current_distance
 			
-			$ScalableRoomInner.linear_scale_from(scale_origin, distance_to_go)
+			transform_target.linear_scale_from(scale_origin, distance_to_go)
 	else:
 		is_scaling = false
 
