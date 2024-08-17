10d0cf929b73283eae17e655ee4f3f1a02d322ee
Cameron Smith <59268172+dumbanode@users.noreply.github.com>
2024-08-17 16:30:08 -0700

    fixed

--- title_screen.gd
+++ title_screen.gd
@@ -1,8 +1,9 @@
 extends Control
 
+@export var scene_to_load_on_play: PackedScene
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://level.tscn")
+	get_tree().change_scene_to_packed(scene_to_load_on_play)
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
