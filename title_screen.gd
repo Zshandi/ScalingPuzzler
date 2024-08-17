81ae1d694d90b7eec25e22150ae7f2416f888256
Cameron Smith <59268172+dumbanode@users.noreply.github.com>
2024-08-17 16:15:03 -0700

    Fuxed

--- title_screen.gd
+++ title_screen.gd
@@ -4,10 +4,5 @@ extends Control
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://level.tscn")
 
-
-func _on_settings_pressed() -> void:
-	pass # Replace with function body.
-
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
