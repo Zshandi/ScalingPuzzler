b7ae488d672a4a7afbbb7a8d5ce6fb870f8cc593
Zshandi Krahn <ZshandiK@gmail.com>
2024-08-17 15:39:34 -0700

    Move scaling logic to separate class
    This way, changing the input that determines the scaling will be much simpler

--- controller.gd
+++ controller.gd
@@ -8,32 +8,31 @@ var scale_factor := 500.0
 func _ready():
 	pass # Replace with function body.
 
-var original_transform := Transform2D.IDENTITY
 var scale_origin := Vector2.ZERO
-var local_scale_origin:Vector2
+var original_scale := 0.0
 var is_scaling := false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		var current_cursor_pos := get_viewport().get_mouse_position()
 		if !is_scaling:
 			scale_origin = current_cursor_pos
-			original_transform = $ScalableRoomInner.global_transform
+			# Assumes equal x and y scale
+			assert($ScalableRoomInner.global_transform.get_scale().x ==
+				$ScalableRoomInner.global_transform.get_scale().y)
+			original_scale = $ScalableRoomInner.get_current_linear_scale()
 			is_scaling = true
 		else:
 			var scale_distance := -(current_cursor_pos.y - scale_origin.y) / scale_factor
-			var scale_val := pow(2, scale_distance)
-			#TODO: Actually change center of scaling
-			var scaled_transform := original_transform.scaled(Vector2.ONE * scale_val)
 			
-			var adjusted_origin := scaled_transform * (original_transform.affine_inverse() * scale_origin)
+			var current_scale:float = $ScalableRoomInner.get_current_linear_scale()
 			
-			var origin_offset := adjusted_origin - scale_origin
+			var current_distance := current_scale - original_scale
 			
-			var final_transform := scaled_transform.translated(-origin_offset)
+			var distance_to_go := scale_distance - current_distance
 			
-			$ScalableRoomInner.global_transform = final_transform
+			$ScalableRoomInner.linear_scale_from(scale_origin, distance_to_go)
 	else:
 		is_scaling = false
