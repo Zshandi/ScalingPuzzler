f13a0d9cda8885ce8392a8b19e9b687879f4bc77
Zshandi Krahn <ZshandiK@gmail.com>
2024-08-17 22:14:31 -0700

    Scroll to scale (#9)
    * Add input actions for each of the transform interactions
    
    * Update level container to be single static body
    
    * Move scaling logic to separate class
    
    * Move transformation target to single variable
    
    * Adjust scaling to use scroll wheel

--- controller.gd
+++ controller.gd
@@ -1,39 +1,76 @@
 extends Node2D
 
 @export
-## The distance which corresponds to doubling or halving the scale
-var scale_factor := 500.0
+## The amount scaled per scroll
+var scale_step := 0.3
+@export
+## The strength of the scale smoothing
+## Note that this corresponds to an exponential decay from the current scale to the target,
+##  meaning it is not really an exact speed, but rather just determines how steep the decay is,
+##  but the exact speed will be dependent on the current distance to the target
+var scale_strength := 5.0
+@export
+var max_scale_speed := 0.2
+
+@export
+## The node to apply transform controls to by default
+var transform_target:Transformable
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Assumes equal x and y scale
+	assert(transform_target.global_transform.get_scale().x ==
+		transform_target.global_transform.get_scale().y)
+	target_scale = transform_target.get_current_linear_scale()
 
-var original_transform := Transform2D.IDENTITY
-var scale_origin := Vector2.ZERO
-var local_scale_origin:Vector2
-var is_scaling := false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
+	handle_scaling(delta)
+	handle_translation(delta)
+	handle_rotation(delta)
+
+#var original_scale := 0.0
+
+var target_scale:float = 0.0
+var scale_direction:int = 0
+var scale_origin := Vector2.ZERO
+
+## Temporary HACK until we actually stop the ball from clipping in #4 (https://github.com/Zshandi/ScalingPuzzler/issues/4)
+var min_scale := -1.2
+
+func handle_scaling(delta):
+	var current_scale:float = transform_target.get_current_linear_scale()
 	
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		var current_cursor_pos := get_viewport().get_mouse_position()
-		if !is_scaling:
-			scale_origin = current_cursor_pos
-			original_transform = $ScalableRoomInner.global_transform
-			is_scaling = true
-		else:
-			var scale_distance := -(current_cursor_pos.y - scale_origin.y) / scale_factor
-			var scale_val := pow(2, scale_distance)
-			#TODO: Actually change center of scaling
-			var scaled_transform := original_transform.scaled(Vector2.ONE * scale_val)
-			
-			var adjusted_origin := scaled_transform * (original_transform.affine_inverse() * scale_origin)
-			
-			var origin_offset := adjusted_origin - scale_origin
-			
-			var final_transform := scaled_transform.translated(-origin_offset)
-			
-			$ScalableRoomInner.global_transform = final_transform
-	else:
-		is_scaling = false
+	if Input.is_action_just_pressed("control_scale_up"):
+		target_scale += scale_step
+		scale_origin = get_viewport().get_mouse_position()
+	if Input.is_action_just_pressed("control_scale_down"):
+		target_scale -= scale_step
+		scale_origin = get_viewport().get_mouse_position()
+	
+	if target_scale != current_scale:
+		var target_scale_direction = 1 if target_scale > 0 else -1
+		
+		# Get new scale (exp decay to target)
+		var scale_to := lerpf(current_scale, target_scale, delta * scale_strength)
+		
+		## Temporary HACK until we actually stop the ball from clipping in #4 (https://github.com/Zshandi/ScalingPuzzler/issues/4)
+		if scale_to < min_scale:
+			scale_to = min_scale
+			target_scale = min_scale
+		
+		# Scale to that scale
+		var scale_by := scale_to - current_scale
+		
+		# Limit scaling speed
+		scale_by = max(-max_scale_speed, scale_by)
+		scale_by = min(max_scale_speed, scale_by)
+		
+		transform_target.linear_scale_from(scale_origin, scale_by)
+
+func handle_translation(delta):
+	pass
+
+func handle_rotation(delta):
+	pass
---
+++ transformable.gd
@@ -0,0 +1,43 @@
+extends Node2D
+class_name Transformable
+
+# Utility helper function, may want to move to static library
+func based_log(base:float = 10, x:float = 10) -> float:
+	return (log(x) / log(base))
+
+func get_current_linear_scale(axis:int = Vector2.AXIS_X) -> float:
+	return based_log(2, global_transform.get_scale()[axis])
+
+
+
+func linear_scale_from(center:Vector2, linear_scale_by:float):
+	# Convenience: go from float to vector
+	linear_scale_from_vec(center, Vector2.ONE * linear_scale_by)
+
+func linear_scale_from_vec(center:Vector2, linear_scale_by:Vector2):
+	# Convert linear to exponential scaling:
+	# 0 = 1x, 1 = 2x, 2 = 4x, -1 = 0.5x, etc.
+	var scale_by := Vector2(pow(2, linear_scale_by.x), pow(2, linear_scale_by.y))
+	scale_from_vec(center, scale_by)
+
+func scale_from(center:Vector2, scale_by:float):
+	# Convenience: go from float to vector
+	scale_from_vec(center, Vector2.ONE * scale_by)
+
+func scale_from_vec(center:Vector2, scale_by:Vector2):
+	# Get original transform for reference
+	var original_transform := global_transform
+	
+	# Simply scale the transform to start
+	var scaled_transform := original_transform.scaled(scale_by)
+	
+	# Get scaled center position:
+	#  1. apply inverse of original transform to get center with no transform (i.e. Identity)
+	#  2. apply new transform to get new center
+	var adjusted_center := scaled_transform * (original_transform.affine_inverse() * center)
+	
+	# Get the difference from original center, so we can counteract this in the final transform
+	var center_offset := adjusted_center - center
+	
+	# Subtract the offset, to counter center point movement from scaling
+	global_transform = scaled_transform.translated(-center_offset)
