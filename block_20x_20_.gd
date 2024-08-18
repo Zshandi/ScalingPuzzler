e17cf0ab0263544e42be51cfbd05555da75d6016
Cameron Smith <59268172+dumbanode@users.noreply.github.com>
2024-08-18 15:14:26 -0700

    17 wall update to use polygon (#21)
    Update walls to use polygon
    
    Add shader to allow texture to repeat
    
    To get texture shape, copy + paste the polygon from the collision

---
+++ block_20x_20_.gd
@@ -0,0 +1,14 @@
+extends CollisionShape2D
+
+@onready var block_texture: TextureRect = $TextureRect
+
+func _ready() -> void:
+	_update_texture_rect_size()
+	
+func _update_texture_rect_size():
+	block_texture.size = shape.extents * 2
+	
+	block_texture.position = Vector2(
+		(block_texture.size.x/2)*-1,
+		(block_texture.size.y/2)*-1
+	)
