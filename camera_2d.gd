a3829e0e2ed2b72a16324bb4838f3b17ebf1f4bd
Zshandi Krahn <ZshandiK@gmail.com>
2024-08-18 18:14:31 -0700

    Add centering

--- camera_2d.gd
+++ camera_2d.gd
@@ -1,29 +1,46 @@
 extends Camera2D
 
 @export
-var pan_max_speed := 50.0
+var pan_max_speed:float = 50.0
 @export
-var pan_accel := 35.0
+var pan_accel:float = 35.0
 @export
-var pan_brake := 45.0
+var pan_brake:float = 45.0
+
+@export
+var pan_center_duration:float = 1.0
+@export
+var pan_center_trans := Tween.TRANS_CUBIC
 
 var pan_speed := Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var pan_to_center:bool = false
 
+var current_center_pan_tween:Tween = null
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	var x_pan := Input.get_axis("camera_pan_left", "camera_pan_right")
-	var y_pan := Input.get_axis("camera_pan_up", "camera_pan_down")
+	if Input.is_action_just_pressed("camera_pan_center"):
+		if current_center_pan_tween != null:
+			current_center_pan_tween.stop()
+		pan_to_center = true
+		
+		var pan_to:Vector2 = $"../Ball".global_position - get_viewport_rect().size / 2
+		
+		current_center_pan_tween = create_tween()
+		current_center_pan_tween.set_trans(pan_center_trans)
+		current_center_pan_tween.tween_property(self, "global_position", pan_to, pan_center_duration)
+		current_center_pan_tween.tween_property(self, "pan_to_center", false, 0.0)
+		current_center_pan_tween.play()
 	
-	adjust_pan_axis(x_pan, Vector2.AXIS_X, delta)
-	adjust_pan_axis(y_pan, Vector2.AXIS_Y, delta)
-	
-	global_position += pan_speed
+	if !pan_to_center:
+		var x_pan := Input.get_axis("camera_pan_left", "camera_pan_right")
+		var y_pan := Input.get_axis("camera_pan_up", "camera_pan_down")
+		
+		adjust_pan_axis(x_pan, Vector2.AXIS_X, delta)
+		adjust_pan_axis(y_pan, Vector2.AXIS_Y, delta)
+		
+		global_position += pan_speed
 
 func adjust_pan_axis(pan_axis_val:float, pan_axis:int, delta:float):
 	var target_speed := pan_axis_val * pan_max_speed
