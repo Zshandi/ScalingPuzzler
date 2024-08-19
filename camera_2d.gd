9441f7733ccd0b58b7dc0e2197d0a870f3047452
Zshandi Krahn <ZshandiK@gmail.com>
2024-08-18 18:43:37 -0700

    13 add camera panning + centering (#23)
    Add Camera Controls (panning & centering)
    Make panning work with regular controls (account for camera position)
    Fix scrolling background somehow (was not updating properly when panning to the right)
    
    Camera controls:
    
    Regular pan up/left/down/right: W/A/S/D
    Pan to center of ball: C
    Lock camera to center of ball: X

---
+++ camera_2d.gd
@@ -0,0 +1,70 @@
+extends Camera2D
+
+@export
+var pan_max_speed:float = 50.0
+@export
+var pan_accel:float = 35.0
+@export
+var pan_brake:float = 45.0
+
+@export
+var pan_center_duration:float = 1.0
+@export
+var pan_center_trans := Tween.TRANS_CUBIC
+
+var pan_speed := Vector2.ZERO
+
+var is_panning_to_center:bool = false
+
+var is_center_pan_locked:bool = false
+
+var current_center_pan_tween:Tween = null
+
+func _process(delta: float) -> void:
+	
+	if Input.is_action_just_pressed("camera_pan_center_lock"):
+		is_center_pan_locked = not is_center_pan_locked
+		if is_center_pan_locked:
+			pan_to_center(pan_center_duration)
+	
+	if Input.is_action_just_pressed("camera_pan_center") and not is_center_pan_locked:
+		pan_to_center(pan_center_duration)
+	
+	if is_center_pan_locked and not is_panning_to_center:
+		global_position = get_ball_position()
+	
+	if not is_panning_to_center and not is_center_pan_locked:
+		var x_pan := Input.get_axis("camera_pan_left", "camera_pan_right")
+		var y_pan := Input.get_axis("camera_pan_up", "camera_pan_down")
+		
+		adjust_pan_axis(x_pan, Vector2.AXIS_X, delta)
+		adjust_pan_axis(y_pan, Vector2.AXIS_Y, delta)
+		
+		global_position += pan_speed
+
+func adjust_pan_axis(pan_axis_val:float, pan_axis:int, delta:float):
+	var target_speed := pan_axis_val * pan_max_speed
+	
+	var accel := pan_accel
+	if target_speed == 0:
+		accel = pan_brake
+	elif (target_speed < 0) != (pan_speed[pan_axis] < 0):
+		accel += pan_brake
+	
+	pan_speed[pan_axis] = move_toward(pan_speed[pan_axis], target_speed, accel * delta)
+
+func get_ball_position():
+	return $"../Ball".global_position - get_viewport_rect().size / 2
+
+func pan_to_center(duration:float):
+	if current_center_pan_tween != null:
+		current_center_pan_tween.stop()
+	is_panning_to_center = true
+	
+	var pan_to:Vector2 = get_ball_position()
+	
+	current_center_pan_tween = create_tween()
+	current_center_pan_tween.set_trans(pan_center_trans)
+	current_center_pan_tween.tween_property(self, "global_position", pan_to, duration)
+	current_center_pan_tween.tween_property(self, "is_panning_to_center", false, 0.0)
+	current_center_pan_tween.play()
