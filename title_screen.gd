0500123bf202ec25e29cda3ba908a368a42e9f13
Cameron Smith <59268172+dumbanode@users.noreply.github.com>
2024-08-20 01:24:35 -0700

    35 add settings menu (#46)
    * Created settings menu
    
    * I feel like Im missing something

--- title_screen.gd
+++ title_screen.gd
@@ -2,8 +2,15 @@ extends Control
 
 var level_order: LevelOrder = preload("res://resources/level_order.tres")
 
+func _ready() -> void:
+	Settings.load_save_data()
+
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_packed(level_order.level_order[0])
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://settings.tscn")
