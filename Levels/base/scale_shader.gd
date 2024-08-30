d87682c66aceca9d43cbed5c32de265269529a57
Zshandi Krahn <ZshandiK@gmail.com>
2024-08-29 21:38:33 -0700

    Minor improvements & checks

--- Levels/base/scale_shader.gd
+++ Levels/base/scale_shader.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
 func _process(delta: float) -> void:
-	var screen_zoom:float = get_viewport().get_camera_2d().zoom.x
-	material.set_shader_parameter("scale", 1.0/screen_zoom)
+	# Make sure the uniform exists on the shader and is a float
+	if material.get_shader_parameter("scale") is float:
+		# Get the current zoom of the camera
+		var screen_zoom:float = get_viewport().get_camera_2d().zoom.x
+		# The scale is the current camera zoom
+		material.set_shader_parameter("scale", 1.0/screen_zoom)
