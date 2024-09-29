e76aa8aca2c404ac3a287a9191577853096dfa9e
Zshandi Krahn <ZshandiK@gmail.com>
2024-09-29 00:05:20 -0700

    Fix some problems with debug commands
    Make debug commands work better:
    
     - Enable scrolling in history
     - Add help
     - ESC properly closes without opening pause menu

--- Levels/base/debug_commands.gd
+++ Levels/base/debug_commands.gd
@@ -5,22 +5,28 @@ signal command_received(cmd_name:String, cmd_args:Array[String])
 @onready
 var command_prompt := $VBoxContainer/CommandPrompt
 @onready
-var command_output := $VBoxContainer/CommandOutput
+var command_output := $VBoxContainer/ScrollContainer/CommandOutput
+@onready
+var scroll := $VBoxContainer/ScrollContainer
+
+var command_history:Array[String] = []
 
 func write_line(output:String):
 	command_output.text += output + "\n"
+	await get_tree().create_timer(0).timeout
+	scroll.get_v_scroll_bar().value = scroll.get_v_scroll_bar().max_value
 
 func process_command(command: String):
 	print_debug("Received command: ", command)
 	
 	var cmd_args:Array[String]
 	cmd_args.assign(command.split(" ", false))
-	var cmd_name := cmd_args[0]
-	cmd_args.pop_front()
+	var cmd_name := ""
+	if (cmd_args.size() > 0):
+		cmd_name = cmd_args[0].to_lower()
+		cmd_args.pop_front()
 	
 	command_received.emit(cmd_name, cmd_args)
-	
-	write_line("/" + command)
 	
 	var error:String = ""
 	
@@ -29,34 +35,39 @@ func process_command(command: String):
 			if cmd_args.size() != 1 or !cmd_args[0].is_valid_int():
 				error = "setlevel requires exactly one integer parameter"
 			else:
+				# TODO: Don't rely on position in tree here
 				$"../..".set_level(cmd_args[0].to_int())
 		"setlevelrelative":
 			if cmd_args.size() != 1 or !cmd_args[0].is_valid_int():
 				error = "setlevelrelative requires exactly one integer parameter"
 			else:
+				# TODO: Don't rely on position in tree here
 				$"../..".set_level_relative(cmd_args[0].to_int())
+		"help":
+			# TODO: Better help system
+			write_line("Available commands: setlevel, setlevelrelative")
+			return
 		_:
-			error = "'" + cmd_name + "' is not recognized as a command"
-	
+			error = "'" + cmd_name + "' is not recognized as a command. Need /help?"
 	
 	if error != "":
 		write_line("ERROR: " + error)
-		command_prompt.text = "/"
-		command_prompt.caret_column = 1
 	else:
 		set_prompt_enabled(false)
-		command_prompt.clear()
 		command_prompt.release_focus()
 
 func _ready() -> void:
 	set_prompt_enabled(false)
 
 func _input(event: InputEvent) -> void:
-	if OS.is_debug_build() and event.is_action("debug_command_open"):
+	if !OS.is_debug_build(): return
+	if event.is_action("debug_command_open") and !visible:
 		set_prompt_enabled(true)
 		command_prompt.grab_focus()
+		get_viewport().set_input_as_handled()
 	
-	if event.is_action("ui_cancel"):
+	if event.is_action("ui_cancel") and visible:
+		await get_tree().create_timer(0).timeout
 		set_prompt_enabled(false)
 		command_prompt.clear()
 		command_prompt.release_focus()
@@ -65,7 +76,12 @@ func set_prompt_enabled(enabled:bool):
 	command_prompt.editable = enabled
 	command_prompt.selecting_enabled = enabled
 	visible = enabled
+	process_mode = PROCESS_MODE_ALWAYS if enabled else PROCESS_MODE_INHERIT
+	get_tree().paused = enabled
 
 func _on_command_prompt_text_submitted(new_text: String) -> void:
+	write_line(new_text)
+	command_prompt.clear()
+	
 	var command := new_text.trim_prefix("/")
 	process_command(command)
