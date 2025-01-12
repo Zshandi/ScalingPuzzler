143905e8d3b16036a3cd6480f7c15b5a9d2000ec
Zshandi Krahn <ZshandiK@gmail.com>
2025-01-12 00:06:36 -0800

    Remove unnecessary comment

--- version.gd
+++ version.gd
@@ -1,7 +1,5 @@
 extends Label
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var version = ProjectSettings.get_setting("application/config/version")
 	#if OS.is_debug_build():
