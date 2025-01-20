a60cf387cca6124db0bb98515e8f0e4e786f89cc
Zshandi Krahn <ZshandiK@gmail.com>
2025-01-19 22:30:08 -0800

    Final updates to build script

--- build.ps1
+++ build.ps1
@@ -12,11 +12,7 @@ $project_version_key = "config/version"
 
 $config = ([xml](Get-Content '.\build_config.xml')).config
 
-$exports = $config.exports
-# Currently a workaround, this should be more dynamic (dictionary?)
-$export_name_web = "Web"
-$export_file_name_web = "index"
-$export_name_exe = "Windows Desktop"
+$exports = $config.exports.export
 
 $version_prefix = $config.version_prefix
 
@@ -24,26 +20,16 @@ $git_main_branch = $config.git_main_branch
 
 $exports_base_dir = $config.exports_base_dir
 
+$godot_command = $config.godot_command
+$additional_command_args = $config.additional_args
+
 if (!(Test-Path -PathType Leaf .\project.godot))
 {
     Write-Error "Couldn't find project.godot: must be run from project root"
     exit 1;
 }
 
-function FindProjectSetting
-{
-    param ( [string] $key )
-
-    $groups = (Select-String -Path .\project.godot -Pattern "$key=""(.*)""").Matches.Groups
-
-    if ($groups){
-        return $groups[1].Value
-    }
-    else
-    {
-        return $null
-    }
-}
+#====  PROJECT NAME  ====#
 
 # Get the project name from project.godot
 $project_name_groups = (Select-String -Path .\project.godot -Pattern "$project_name_key=""(.*)""").Matches.Groups
@@ -57,6 +43,8 @@ else
     Write-Error "Invalid project.godot: missing or empty project name ($project_name_key)"
     exit 1;
 }
+
+#====  VERSION - GET AND UPDATE  ====#
 
 # Get the version from project.godot
 # Note that this must end with a number, which is what will be considered the build number
@@ -78,7 +66,7 @@ if ($version_groups)
 
         $version_string = "$version_string $commit"
     }
-    else
+    elseif (!$NoVersionIncrement)
     {
         # Update the version if on main branch
         $rev_number = $version_groups[3].Value
@@ -88,7 +76,7 @@ if ($version_groups)
 
         $new_version = "$rest$new_rev_number"
 
-        Write-Host "Updating project version ($project_version_key) from $rest$rev_number to $new_version"
+        Write-Host "Updating project version ($project_version_key) from $rest$rev_number to $new_version" -ForegroundColor Green
 
         (Get-Content .\project.godot) `
             -replace "$project_version_key="".*""", "$project_version_key=""$new_version""" |
@@ -97,37 +85,30 @@ if ($version_groups)
 }
 else
 {
-    Write-Host "Couldn't find project version ($project_version_key), ommitting from export path"
+    Write-Host "Couldn't find project version ($project_version_key), ommitting from export path" -ForegroundColor Red
     $version_string = ""
 }
+
+#====  CREATE BASE DIRECTORIES  ====#
 
 $export_versioned_dir = "$exports_base_dir\$project_name$version_string"
 
 New-Item -Path $export_versioned_dir -ItemType Directory
 
-foreach ($match in (Select-String -Path .\export_presets.cfg -Pattern "name=""(.*)""").Matches)
+
+#====  EXPORT TEAMPLATES FROM CONFIG  ====#
+
+foreach ($export in $exports)
 {
-    $export_name = $match.Groups[1].Value
+    $export_name = $export.name
 
-    if ("" -eq $export_name)
-    {
-        continue
-    }
+    $project_and_version = "$project_name$version_string"
 
-    $export_file_name = "$project_name$version_string - $export_name"
+    $export_file_name = "$project_and_version - $export_name"
 
     $export_zip_file = "$export_file_name.zip"
 
-    if ($export_name_web -eq $export_name)
-    {
-        # Currently a workaround, this should be configurable
-        $export_file_name = $export_file_name_web
-    }
-    elseif ($export_name_exe -eq $export_name)
-    {
-        # Currently a workaround, this should be configurable
-        $export_file_name = "$export_file_name.exe"
-    }
+    $export_file_name = $export.file_name -replace "\[project_and_version\]", "$project_and_version"
 
     $export_path = "$export_versioned_dir\$export_name"
     $export_file_path = "$export_path\$export_file_name"
@@ -138,8 +119,12 @@ foreach ($match in (Select-String -Path .\export_presets.cfg -Pattern "name=""(.
         $export_switch = "--export-debug"
     }
 
+    Write-Host "Creating directory for export '$export_name': $export_path" -ForegroundColor Green
     New-Item -Path "$export_path" -ItemType Directory
-    godot --headless $export_switch "$export_name" "$export_file_path"
+
+    Write-Host "Exporting for '$export_name', to file $export_file_path" -ForegroundColor Green
+    Start-Process -Wait "$godot_command" "$export_switch ""$export_name"" ""$export_file_path"" $additional_command_args"
     
+    Write-Host "Compressing contents of '$export_path\' to '$export_versioned_dir\$export_zip_file'" -ForegroundColor Green
     Compress-Archive "$export_path\*" -DestinationPath "$export_versioned_dir\$export_zip_file"
 }
